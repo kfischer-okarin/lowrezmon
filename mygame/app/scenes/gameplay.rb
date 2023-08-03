92ead8d5d3597724e18d97c8d3f5d563e042bf7f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-08-03 13:20:56 +0900

    Add prototype sprites

--- mygame/app/scenes/gameplay.rb
+++ mygame/app/scenes/gameplay.rb
@@ -4,10 +4,30 @@ module Scenes
     end
 
     def render(screen, state)
-      screen.primitives << build_label(
-        x: 32, y: 32, text: 'Hello, Lowrez!', r: 255, g: 255, b: 255,
-        alignment_enum: 1, vertical_alignment_enum: 1
-      )
+      @font = build_pokemini_font
+      screen.primitives << {
+        x: 0, y: 0, w: 64, h: 64, path: :pixel,
+        r: 253, g: 253, b: 230
+      }.sprite!
+      screen.primitives << {
+        x: 40, y: 40, w: 16, h: 16, path: 'sprites/emojis.png',
+        source_x: 0, source_y: 0, source_w: 16, source_h: 16
+      }.sprite!
+      screen.primitives << {
+        x: 4, y: 19, w: 23, h: 18, path: 'sprites/blank_emojis.png',
+        source_x: 0, source_y: 4, source_w: 16, source_h: 12
+      }.sprite!
+      # window
+      screen.primitives << {
+        x: 0, y: 18, w: 64, h: 1, path: :pixel,
+        r: 0, g: 0, b: 0
+      }.sprite!
+      screen.primitives << {
+        x: 1, y: 1, w: 62, h: 17, path: :pixel,
+        r: 255, g: 255, b: 255
+      }.sprite!
+      screen.primitives << @font.build_label(text: 'A wild angry', x: 2, y: 10)
+      screen.primitives << @font.build_label(text: 'emoji appears!', x: 2, y: 2)
     end
   end
 end
