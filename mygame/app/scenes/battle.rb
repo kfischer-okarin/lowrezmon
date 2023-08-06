6eb946cd7df87dccc26eec92e8129c55700a93a7
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-08-06 09:04:29 +0900

    Add initial send emojimon messages + animations

--- mygame/app/scenes/battle.rb
+++ mygame/app/scenes/battle.rb
@@ -3,13 +3,16 @@ module Scenes
     def initialize(player:, opponent:)
       @player = player
       @opponent = opponent
-      @player_emojimon = build_emojimon @player[:emojimons].first
-      @opponent_emojimon = build_emojimon @opponent[:emojimons].first
+      @player_emojimon = nil
+      @player_emojimon_sprite = nil
+      @opponent_emojimon = nil
+      @opponent_emojimon_sprite = nil
       @line0_letters = []
       @line1_letters = []
       @line_index = 0
       @cutscene = Cutscene.build_empty
       @state = :battle_start
+      @queued_states = []
       @font = build_pokemini_font
       @waiting_for_advance_message_since = nil
     end
@@ -32,8 +35,20 @@ module Scenes
 
       case @state
       when :battle_start
-        queue_message("#{@opponent[:name]} wants to battle!")
+        queue_message("#{@opponent[:name]} wants to battle!", tick: @tick_count + 60)
         @state = :opponent_sends_emojimon
+        @queued_states = [:player_sends_emojimon, :player_chooses_action]
+      when :opponent_sends_emojimon
+        @opponent_emojimon = build_emojimon @opponent[:emojimons].first
+        queue_message("#{@opponent[:name]} sends #{@opponent_emojimon[:name]}!")
+        queue_opponent_emojimon_appearance
+        @state = @queued_states.shift
+      when :player_sends_emojimon
+        @player_emojimon = build_emojimon @player[:emojimons].first
+        queue_message("Go, #{@player_emojimon[:name]}!")
+        queue_player_emojimon_appearance
+        @state = @queued_states.shift
+      when :player_chooses_action
       end
     end
 
@@ -42,8 +57,8 @@ module Scenes
         x: 0, y: 0, w: 64, h: 64, path: :pixel,
         r: 0xF0, g: 0xD0, b: 0xB0
       }.sprite!
-      screen.primitives << @opponent_emojimon[:sprite].to_sprite(x: 40, y: 40)
-      screen.primitives << @player_emojimon[:back_sprite].to_sprite(x: 4, y: 19)
+      screen.primitives << @opponent_emojimon_sprite
+      screen.primitives << @player_emojimon_sprite
       render_window(screen)
     end
 
@@ -91,6 +106,14 @@ module Scenes
       Cutscene.schedule_element @cutscene, tick: tick, type: :state_change, duration: 1, new_state: new_state
     end
 
+    def queue_opponent_emojimon_appearance(tick: @tick_count + 1)
+      Cutscene.schedule_element @cutscene, tick: tick, type: :opponent_emojimon_appears, duration: 60
+    end
+
+    def queue_player_emojimon_appearance(tick: @tick_count + 1)
+      Cutscene.schedule_element @cutscene, tick: tick, type: :player_emojimon_appears, duration: 60
+    end
+
     def message_tick(_args, message_element)
       if message_element[:line_index].zero?
         y = 11
@@ -110,6 +133,40 @@ module Scenes
 
     def wait_for_advance_message_tick(_args, _element)
       @waiting_for_advance_message_since = @tick_count
+    end
+
+    def opponent_emojimon_appears_tick(_args, element)
+      case element[:elapsed_ticks]
+      when 0
+        target_values = @opponent_emojimon[:sprite].slice(:h, :tile_h).merge(r: 255, g: 255, b: 255)
+        @opponent_emojimon_sprite = @opponent_emojimon[:sprite].to_sprite(
+          x: 40, y: 40, h: 0, tile_h: 0, r: 0, g: 0, b: 0
+        )
+        element[:grow_animation] = Animations.lerp(
+          @opponent_emojimon_sprite,
+          to: target_values,
+          duration: 60
+        )
+      else
+        Animations.perform_tick element[:grow_animation]
+      end
+    end
+
+    def player_emojimon_appears_tick(_args, element)
+      case element[:elapsed_ticks]
+      when 0
+        target_values = @player_emojimon[:back_sprite].slice(:h, :source_h).merge(r: 255, g: 255, b: 255)
+        @player_emojimon_sprite = @player_emojimon[:back_sprite].to_sprite(
+          x: 4, y: 19, h: 0, source: 0, r: 0, g: 0, b: 0
+        )
+        element[:grow_animation] = Animations.lerp(
+          @player_emojimon_sprite,
+          to: target_values,
+          duration: 60
+        )
+      else
+        Animations.perform_tick element[:grow_animation]
+      end
     end
   end
 end
