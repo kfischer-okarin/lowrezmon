e99d833c0533d0bf8acc888dcce584a6a57869a4
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-08-11 15:29:32 +0900

    Extract char_sprite method for SpritesheetFont

--- mygame/lib/spritesheet_font.rb
+++ mygame/lib/spritesheet_font.rb
@@ -15,15 +15,21 @@ class SpritesheetFont
     @letter_positions.merge!(letter_positions)
   end
 
+  def char_sprite(char)
+    letter_position = @letter_positions[char]
+    {
+      w: letter_position[:tile_w], h: letter_position[:tile_h],
+      path: @path,
+      r: 0, g: 0, b: 0
+    }.sprite!(letter_position)
+  end
+
   def build_label(text:, x:, y:, **values)
     result = []
     text.each_char do |char|
       letter_position = @letter_positions[char]
-      letter_sprite = {
-        x: x, y: y, w: letter_position[:tile_w], h: letter_position[:tile_h],
-        path: @path,
-        r: 0, g: 0, b: 0
-      }.sprite!(letter_position).merge!(values)
+      letter_sprite = char_sprite(char).merge!(x: x, y: y)
+                                       .merge!(values)
       result << letter_sprite
       x += letter_sprite[:w] + 1
     end
